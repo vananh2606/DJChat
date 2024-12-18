c9780f7aa240e72e7f42a5d5c39b294e685b82fb
vananh2606 <66019139+vananh2606@users.noreply.github.com>
2024-12-18 10:38:22 +0700

    Update_Frontend

---
+++ reactchat/src/pages/templates/PrimaryAppBar.tsx
@@ -0,0 +1,90 @@
+import {
+    AppBar,
+    Box,
+    Drawer,
+    IconButton,
+    Link,
+    Toolbar,
+    Typography,
+    useMediaQuery
+} from "@mui/material"
+import { useTheme } from "@mui/material/styles"
+import MenuIcon from "@mui/icons-material/Menu"
+import { useEffect, useState } from "react"
+
+const PrimaryAppBar = () => {
+    const [sideMenu, setSideMenu] = useState(false)
+    const theme = useTheme()
+
+    const isSmallScreen = useMediaQuery(theme.breakpoints.up("sm"))
+
+    useEffect(() => {
+        if (isSmallScreen && sideMenu) {
+            setSideMenu(false)
+        }
+    }, [isSmallScreen])
+
+    const toggleDrawer = (open: boolean) =>
+        (event: React.KeyboardEvent | React.MouseEvent) => {
+            if (
+                event.type === "keydown" &&
+                ((event as React.KeyboardEvent).key === "Tab" || (event as React.KeyboardEvent).key === "Shift")
+            ) {
+                return
+            }
+            setSideMenu(open)
+        }
+
+    return (
+        <AppBar sx={{
+            zIndex: (theme) => theme.zIndex.drawer + 2,
+            backgroundColor: theme.palette.background.default,
+            borderBottom: `1px solid ${theme.palette.divider}`
+        }}>
+            <Toolbar
+                variant="dense"
+                sx={{
+                    height: theme.primaryAppBar.height
+                }}
+            >
+
+                <Box sx={{
+                    display: { xs: "block", sm: "none" }
+                }}
+                >
+                    <IconButton
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={toggleDrawer(true)}
+                        sx={{ mr: 2 }}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                </Box>
+
+                <Drawer anchor="left" open={sideMenu} onClose={toggleDrawer(false)}>
+                    {[...Array(100)].map((_, i) => (
+                        <Typography key={i} paragraph>
+                            {i + 1}
+                        </Typography>
+                    ))}
+                </Drawer>
+
+                <Link href='/' underline="none">
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="div"
+                        sx={{
+                            display: { fontWeight: 700, letterSpacing: "-0.5px" }
+                        }}
+                    >
+                        DJCHAT
+                    </Typography>
+                </Link>
+            </Toolbar>
+        </AppBar >
+    )
+}
+
+export default PrimaryAppBar
---
+++ reactchat/src/theme/theme.tsx
@@ -0,0 +1,63 @@
+import { createTheme, responsiveFontSizes } from "@mui/material";
+
+declare module "@mui/material/styles" {
+    interface Theme {
+        primaryAppBar: {
+            height: number;
+        }
+        primaryDraw: {
+            width: number;
+            closed: number;
+        }
+        secondaryDraw: {
+            width: number;
+        }
+    }
+    interface ThemeOptions {
+        primaryAppBar?: {
+            height?: number;
+        }
+        primaryDraw?: {
+            width?: number;
+            closed?: number;
+        }
+        secondaryDraw?: {
+            width?: number;
+        }
+    }
+}
+
+export const createMuiTheme = () => {
+    let theme = createTheme({
+
+        typography: {
+            fontFamily: ['IBM Plex Sans', 'sans-serif'].join(","),
+        },
+
+        primaryAppBar: {
+            height: 50,
+        },
+
+        primaryDraw: {
+            width: 240,
+            closed: 70,
+        },
+
+        secondaryDraw: {
+            width: 240,
+        },
+
+        components: {
+            MuiAppBar: {
+                defaultProps: {
+                    color: "default",
+                    elevation: 0
+                }
+            }
+        }
+    })
+    theme = responsiveFontSizes(theme)
+    return theme
+}
+
+export default createMuiTheme
